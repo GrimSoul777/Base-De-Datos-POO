9cbea898dc7e44c062275f64db615a3289da364b
valen-zmg <v0754437@gmail.com>
2026-07-15 18:46:27 -0600

    [ADD] se agrego cambios al intertarjeta

--- src/INTERFACES/InterTarjetaDAO.java
+++ src/INTERFACES/InterTarjetaDAO.java
@@ -1,5 +1,9 @@
 package INTERFACES;
 
 public interface InterTarjetaDAO {
-
+    boolean guardar(Tarjeta tarjeta);
+    ArrayList<Tarjeta> listar();
+    Tarjeta buscar(int id);
+    //boolean editar(Tarjeta tarjeta);
+    boolean eliminar(int id);
 }
